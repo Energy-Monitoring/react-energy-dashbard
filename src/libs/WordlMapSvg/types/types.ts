4a96efb2a69e52913eb2aa74c155d55662666b12
Björn Hempel <bjoern@hempel.li>
2024-07-15 00:00:58 +0200

    Add type to InterfaceGeoJson

--- src/libs/WordlMapSvg/types/types.ts
+++ src/libs/WordlMapSvg/types/types.ts
@@ -48,6 +48,7 @@ export type TypeFeature = {
 };
 
 export interface InterfaceGeoJson extends FeatureCollection {
+    type: "FeatureCollection";
     citiesAdded?: boolean;
     features: TypeFeature[];
 }
